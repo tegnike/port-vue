fbd1354c2387864e6b6e1c093a211e178c9a2273
tegnike <user@mbp.local>
2020-03-31 09:14:10 +0200

    add bootstrap & view test

--- portfolio/src/router/index.js
+++ portfolio/src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue' // added
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import 'bootstrap/dist/css/bootstrap.css' // added
+import 'bootstrap-vue/dist/bootstrap-vue.css' // added
 
 Vue.use(Router)
+Vue.use(BootstrapVue) // added
 
 export default new Router({
   routes: [
