e00dc590af099df3c5673a7a5caf937d8248f22b
tegnike <user@mbp.local>
2020-03-31 21:33:50 +0200

    add contact

--- portfolio/src/components/contact.vue
+++ portfolio/src/components/contact.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>contact</h1>
+  <div id="contacts">
+    <h1>Contacts</h1>
+    <p>お問い合わせは、下記のTwitterアカウントへDMをしてください。</p>
+    <a href="https://twitter.com/teg_nike" class="btn text-white mt-4 twitter">Twitterへ</a>
+  </div>
 </template>
+
+<style scoped>
+.twitter {
+  background-color: #41b883;
+}
+</style>
