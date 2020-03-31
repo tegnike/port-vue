4971ff758d5544e97d34e7b5794ae9b48bd2c78f
tegnike <user@mbp.local>
2020-03-31 18:30:09 +0200

    add business careers

--- portfolio/src/components/careers.vue
+++ portfolio/src/components/careers.vue
@@ -1,3 +1,104 @@
 <template>
-  <h1>Business Careers</h1>
+  <div class="careers">
+    <h1>Business Careers</h1>
+    <div class="col-lg-10 my-4 mx-auto">
+      <div class="card">
+      <h3 class="card-header"><span>2018年7月〜</span> メビックス株式会社</h3>
+      <div class="card-body">
+        <h4 class="card-title">2018年7月〜 / 治験の患者登録サービスの開発・運用</h4>
+        <div class="content">
+          <p class="card-text">
+            【概要】 <br>
+            本システムは治験に参加する患者データを登録するためのサービスで、製薬会社・医
+            療機関からの要望を基に試験ごとにカスタマイズして利用されます。 私は主に小規模な案件でリーダーとして、クライアントとの打ち合わせからリリース後の
+            運用まで一連の業務を担当しています。
+          </p>
+          <p class="card-text">
+            【OS】 <br>
+            Windows10
+          </p>
+          <p class="card-text">
+            【言語】 <br>
+            Java8、C#
+          </p>
+          <p class="card-text">
+            【DB】 <br>
+            PostgreSQL 9.6
+          </p>
+          <p class="card-text">
+            【その他ツール】<br>
+             Eclipse, Visual Studio, Gitlab
+          </p>
+          <p class="card-text">
+            【メンバー数】<br>
+            2~5名
+          </p>
+          <p class="card-text">
+            【役割】<br>
+             メンバー、サブリーダ ー、リーダー(案件によってアサイン 数、役割は異なる)
+          </p>
+          <p class="card-text">
+            【業務内容】 <br>
+            ・クライアントから要件のヒアリング、仕様書作成
+            ・要件を基に XML を実装、既存コードで賄えない要望については追加開発 ・テスト仕様書の作成、実装、実施 ・リリース後の運用・保守、運用中の要望に対する追加実装
+            ・データ納品
+          </p>
+        </div>
+        <hr>
+        <h4 class="card-title">2018年7月〜 / 治験の患者登録サービスの開発・運用</h4>
+        <div class="content">
+          <p class="card-text">
+            【概要】 <br>
+            子会社が契約しているアルバイターへの業務発注および受注を管理するシステムで、
+            子会社からの依頼で弊社 IT 部門が開発を担当しています。 私は途中からの本プロジェクトへのアサインでしたが、現在は業務の大部分を引き継
+            ぎ、クライアントとの打ち合わせでは、主立って仕様の調整をしています。
+          </p>
+          <p class="card-text">
+            【OS】 <br>
+            Windows10
+          </p>
+          <p class="card-text">
+            【言語・FW】 <br>
+            Ruby 2.5、Rails 5.1
+          </p>
+          <p class="card-text">
+            【DB】 <br>
+            PostgreSQL 9.6
+          </p>
+          <p class="card-text">
+            【その他ツール】<br>
+             Visual Studio Code, Gitlab
+          </p>
+          <p class="card-text">
+            【メンバー数】<br>
+            3名
+          </p>
+          <p class="card-text">
+            【役割】<br>
+             メンバー
+          </p>
+          <p class="card-text">
+            【業務内容】 <br>
+            ・クライアントからのヒアリング、要件定義
+            ・仕様調整後の設計、開発、テスト
+            ・画面 IA の作成
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
+
+<style scoped>
+h3 > span {
+  font-size: 1.2rem;
+}
+.card-text {
+  text-align: left;
+}
+hr {
+  margin-top: 1.5rem;
+  margin-bottom: 2rem;
+}
+</style>
