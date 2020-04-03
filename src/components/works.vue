1218adb45281675ff2609cc4df7c5f0a9b239b00
tegnike <user@mbp.local>
2020-04-03 08:38:10 +0200

    fix error that can't click buttons

--- src/components/works.vue
+++ src/components/works.vue
@@ -1,34 +1,40 @@
 <template>
   <div id="works">
     <h1>Works</h1>
-    <div class="col-lg-9 my-4 mx-auto card-parent">
-      <b-card header="PORT（ポートフォリオ共有サイト）">
-        <b-card-text class="text-left">
-          　私が最初に作ったサービスです。エンジニアが自身のポートフォリオを共有することができます。 他のエンジニアのポートフォリオを参考にしたり、レビューを依頼することで、ポートフォリオのブラッシュアップに活用できます。
-        </b-card-text>
-        <b-card-text class="text-left">
-          【主な使用技術】<br>
-          Ruby 2.5.3, Rails 5.2.3, SASS, Bootstrap, JQuery<br>
-          Dockerfile/docker-compose.yml<br>
-          Redis, Webpacker, RSpec<br>
-          CircleCi, AWS(ECS, ECRなど)
-        </b-card-text>
-        <a href="https://github.com/tegnike/PORT" target="_blank"><b-button class="m-3">Github</b-button></a>
-        <a href="https://port-port.herokuapp.com/" target="_blank"><b-button variant="primary" class="m-3">Webサイト</b-button></a>
-      </b-card>
-      <b-card header="ポートフォリオサイト">
-        <b-card-text class="text-left">
-          　このサイトです。SPAを作成してみたかったので、Vue.jsを使用しています。
-        </b-card-text>
-        <b-card-text class="text-left">
-          【主な使用技術】<br>
-          Vue.js 2.6.1<br>
-          Dockerfile/docker-compose.yml<br>
-          Github Pages
-        </b-card-text>
-        <a href="https://github.com/tegnike/port-vue" target="_blank"><b-button class="m-3">Github</b-button></a>
-      </b-card>
-    </div>
+    <div class="col-lg-9 my-4 mx-auto">
+     <div class="card mb-4">
+       <h3 class="card-header">PORT（ポートフォリオ共有サイト）</h3>
+       <div class="card-body">
+         <p class="card-text text-left">
+            　私が最初に作ったサービスです。エンジニアが自身のポートフォリオを共有することができます。 他のエンジニアのポートフォリオを参考にしたり、レビューを依頼することで、ポートフォリオのブラッシュアップに活用できます。
+         </p>
+         <p class="card-text text-left">
+           【主な使用技術】<br>
+           Ruby 2.5.3, Rails 5.2.3, SASS, Bootstrap, JQuery<br>
+           Dockerfile/docker-compose.yml<br>
+           Redis, Webpacker, RSpec<br>
+           CircleCi, AWS(ECS, ECRなど)
+         </p>
+         <a href="https://github.com/tegnike/PORT" target="_blank"><b-button class="m-3">Github</b-button></a>
+         <a href="https://port-port.herokuapp.com/" target="_blank"><b-button variant="primary" class="m-3">Webサイト</b-button></a>
+       </div>
+     </div>
+     <div class="card">
+       <h3 class="card-header">ポートフォリオサイト</h3>
+       <div class="card-body">
+         <p class="card-text text-left">
+           　このサイトです。SPAを作成してみたかったので、Vue.jsを使用しています。
+         </p>
+         <p class="card-text text-left">
+           【主な使用技術】<br>
+           Vue.js 2.6.1<br>
+           Dockerfile/docker-compose.yml<br>
+           Github Pages
+         </p>
+         <a href="https://github.com/tegnike/port-vue" target="_blank"><b-button class="m-3">Github</b-button></a>
+       </div>
+     </div>
+   </div>
   </div>
 </template>
 
